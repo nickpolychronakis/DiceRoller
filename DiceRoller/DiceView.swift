31ab6782d9a4ee33fa9ce37865702d72a2752206
Nick Polychronakis <nickpolychronakis@me.com>
2019-12-29 14:28:19 +0200

    Dice rolling animation improved with 3d effect.

--- DiceRoller/DiceView.swift
+++ DiceRoller/DiceView.swift
@@ -10,21 +10,36 @@ import SwiftUI
 
 /// A View that draws a dice with a specific number
 struct DiceView: View {
+    // Number of the dice
     let diceNumber: Int
+    // The size of the dice
     let sizeOfDices: CGFloat = 80.0
+    // the angle of the rotation of the dices
+    @State private var angleOfDice: Double = 0.0
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width:sizeOfDices, height: sizeOfDices)
-                .shadow(radius: 10)
-                .foregroundColor(.white)
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width:sizeOfDices, height: sizeOfDices)
+                    .shadow(radius: 10)
+                    .foregroundColor(.white)
 
-            Text("\(diceNumber)")
-                .font(.largeTitle)
-                .foregroundColor(.red)
+                Text("\(diceNumber)")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+            }
+        }
+        .rotation3DEffect(.degrees(self.angleOfDice), axis: (x: 1, y: 0, z: 0))
+        .animation(Animation.default.speed(0.3))
+        .onAppear {
+            self.angleOfDice += 720
         }
     }
+
 }
+
+
 
 struct Dice_Previews: PreviewProvider {
     static var previews: some View {
